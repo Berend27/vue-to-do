d04752b6f31078af9147157b79b5678edeaaaabb
Dexter Berend <dexterjberend@gmail.com>
2023-08-28 06:46:07 -0500

    displays hard coded titles

---
+++ src/components/Todo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="todo">
+    <h2>{{ todo.title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,36 @@
+<!-- <template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template> -->
+
+<template>
+  <div class="home">
+    <h1>Todos for today</h1>
+    <Todo v-for="todo in todos" :key="todo.title" :todo="todo" />
+  </div>
+</template>
+
+<script>
+import Todo from "@/components/Todo.vue";
+
+export default {
+  name: "HomePage",
+  components: {
+    Todo,
+  },
+  data() {
+    return {
+      todos: null,
+    };
+  },
+  created() {
+    this.todos = [
+      { title: "Finish the todo app" },
+      { title: "Walk Peanut" },
+      { title: "Study German" },
+    ];
+  },
+};
+</script>
