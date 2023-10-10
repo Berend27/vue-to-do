bbd17dd29efff992c1eae412a3eb7f5e91e0dc0f
Dexter Berend <dexterberend@gmail.com>
2023-10-09 21:33:28 -0500

    a modal pops up, though a form is needed

---
+++ src/components/Modal.vue
@@ -0,0 +1,52 @@
+<script> 
+  export default {
+    props: {
+      knopftext: String,
+      show: Boolean, 
+    }
+  }
+</script>
+
+<template>
+  <Transition name="modal">
+    <div v-if="show" class="modal-mask">
+      <div class="modal-container">
+        <div class="modal-header">
+          <slot name="header"></slot>
+        </div>
+        <div class="modal-footer">
+          <slot name="footer">
+            <button 
+              class="Fußzeille-button"
+              @click="$emit('close')"
+            >{{ knopftext }}</button>
+          </slot>
+        </div>
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<style scoped>
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  transition: opacity 0.3s ease;
+}
+
+.modal-container {
+  width: 300px;
+  margin: auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -9,22 +9,36 @@
   <div class="home">
     <h1>Todos for today</h1>
     <Todo v-for="todo in todos" :key="todo.title" :todo="todo" />
+    <button id="show-modal" @click="showModal = true; console.log('+ clicked');">+</button>
+    <Teleport to="body">
+      <modal knopftext="Addiere" :show="showModal" @close="showModal = false">
+        <template #header>
+          <h3>Add a Task</h3>
+        </template>
+      </modal>
+    </Teleport>
   </div>
 </template>
 
 <script>
+import Modal from "@/components/Modal.vue";
 import Todo from "@/components/Todo.vue";
 import axios from "axios";
 
 export default {
   name: "HomePage",
   components: {
+    Modal,
     Todo,
   },
   data() {
     return {
+      showModal: false,
       todos: null,
     };
+  },
+  methods: {
+
   },
   created() {
     axios
